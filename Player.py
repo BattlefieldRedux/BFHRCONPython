9273f170e6181d63d91fc23792cd7fd614ee2137
Scott Davey <me@sd149.co.uk>
2013-05-18 21:25:39 +0100

    Privatised Attributes of Player Class
    Attributes should be accessed with appropriate "Get" methods.

--- Player.py
+++ Player.py
@@ -40,77 +40,77 @@ class Player:
     owner: bool, false by default
     """
     def __init__(self, _id, name, kit, kills, deaths, suicides, team, level, score, ping, ip, playerid, vip, heroid, mod=False, admin=False, owner=False):
-        self.id = _id
-        self.alive = True
-        self.name = name
+        self.__id = _id
+        self.__alive = True
+        self.__name = name
         if kit == "NA_Gunner_kit" or kit == "RA_Gunner_kit":
-            self.kit = "Gunner"
+            self.__kit = "Gunner"
         elif kit == "RA_Soldier_kit" or kit == "NA_Soldier_kit":
-            self.kit = "Soldier"
+            self.__kit = "Soldier"
         elif kit == "RA_Commando_kit" or kit == "NA_Commando_kit":
-            self.kit = "Commando"
+            self.__kit = "Commando"
         elif kit == "none":
-            self.kit = "Dead"
-            self.alive = False
-        self.kills = kills
-        self.deaths = deaths
-        self.suicides = suicides
+            self.__kit = "Dead"
+            self.__alive = False
+        self.__kills = kills
+        self.__deaths = deaths
+        self.__suicides = suicides
         if team == 1:
-            self.team = "National"
+            self.__team = "National"
         elif team == 2:
-            self.team = "Royal"
-        self.level = level
-        self.score = score
-        self.ping = ping
-        self.ip = ip
-        self.playerid = playerid
+            self.__team = "Royal"
+        self.__level = level
+        self.__score = score
+        self.__ping = ping
+        self.__ip = ip
+        self.__playerid = playerid
         if vip == 1:
-            self.vip = True
+            self.__vip = True
         else:
-            self.vip = False
-        self.heroid = heroid
-        self.mod = mod
-        self.admin = admin
-        self.owner = owner
+            self.__vip = False
+        self.__heroid = heroid
+        self.__mod = mod
+        self.__admin = admin
+        self.__owner = owner
 
     def getId(self):
-        return self.id
+        return self.__id
     def getName(self):
-        return self.name
+        return self.__name
     def isAlive(self):
-        return self.alive
+        return self.__alive
     def getKit(self):
-        return self.kit
+        return self.__kit
     def getKills(self):
-        return self.kills
+        return self.__kills
     def getDeaths(self):
-        return self.deaths
+        return self.__deaths
     def getSuicides(self):
-        return self.suicides
+        return self.__suicides
     def getTeam(self):
-        return self.team
+        return self.__team
     def getLevel(self):
-        return self.level
+        return self.__level
     def getScore(self):
-        return self.score
+        return self.__score
     def getPing(self):
-        return self.ping
+        return self.__ping
     def getIp(self):
-        return self.ip
+        return self.__ip
     def getPlayerId(self):
-        return self.playerid
+        return self.__playerid
     def isVip(self):
-        return self.vip
+        return self.__vip
     def getHeroId(self):
-        return self.heroid
+        return self.__heroid
     def getDeathsSuicides(self):
-        return self.deaths-self.suicides
+        return self.__deaths-self.__suicides
     def isMod(self):
-        return self.mod
+        return self.__mod
     def isAdmin(self):
-        return self.admin
+        return self.__admin
     def isOwner(self):
-        return self.owner
+        return self.__owner
 
 
 
