19c6a5a85ddc330f30d3e5cb9d2ba70067aaf4a9
Scott Davey <me@sd149.co.uk>
2013-05-18 21:10:27 +0100

    Updated Server Base + New Player Class

---
+++ Player.py
@@ -0,0 +1,123 @@
+__author__ = 'Scott Davey'
+
+"""
+Base for connecting and querying BFH/BFP4F/BF2 servers using RCON.
+Copyright (C) 2013  Scott Davey, www.sd149.co.uk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+class Player:
+    """
+    Representing a player, includes other options for admin tools with their own built in permissions.
+    _id: int
+    name: str
+    kit: str
+    kills: int
+    deaths: int
+    suicides: int
+    team: int
+    level: int
+    score: int
+    ping: int
+    ip: str
+    playerid: int
+    vip: int
+    heroid: int
+    mod: bool, false by default
+    admin: bool, false by default
+    owner: bool, false by default
+    """
+    def __init__(self, _id, name, kit, kills, deaths, suicides, team, level, score, ping, ip, playerid, vip, heroid, mod=False, admin=False, owner=False):
+        self.id = _id
+        self.alive = True
+        self.name = name
+        if kit == "NA_Gunner_kit" or kit == "RA_Gunner_kit":
+            self.kit = "Gunner"
+        elif kit == "RA_Soldier_kit" or kit == "NA_Soldier_kit":
+            self.kit = "Soldier"
+        elif kit == "RA_Commando_kit" or kit == "NA_Commando_kit":
+            self.kit = "Commando"
+        elif kit == "none":
+            self.kit = "Dead"
+            self.alive = False
+        self.kills = kills
+        self.deaths = deaths
+        self.suicides = suicides
+        if team == 1:
+            self.team = "National"
+        elif team == 2:
+            self.team = "Royal"
+        self.level = level
+        self.score = score
+        self.ping = ping
+        self.ip = ip
+        if playerid[-1]  == "\\":
+            self.playerid = playerid[:-1]
+        else:
+            self.playerid = playerid
+        if vip == "1":
+            self.vip = True
+        else:
+            self.vip = False
+        self.heroid = heroid
+        self.mod = mod
+        self.admin = admin
+        self.owner = owner
+
+    def getId(self):
+        return self.id
+    def getName(self):
+        return self.name
+    def isAlive(self):
+        return self.alive
+    def getKit(self):
+        return self.kit
+    def getKills(self):
+        return self.kills
+    def getDeaths(self):
+        return self.deaths
+    def getSuicides(self):
+        return self.suicides
+    def getTeam(self):
+        return self.team
+    def getLevel(self):
+        return self.level
+    def getScore(self):
+        return self.score
+    def getPing(self):
+        return self.ping
+    def getIp(self):
+        return self.ip
+    def getPlayerId(self):
+        return self.playerid
+    def isVip(self):
+        return self.vip
+    def getHeroId(self):
+        return self.heroid
+    def getDeathsSuicides(self):
+        return int(self.deaths)-int(self.suicides)
+    def isMod(self):
+        return self.mod
+    def isAdmin(self):
+        return self.admin
+    def isOwner(self):
+        return self.owner
+    def getRank(self):
+        if self.owner: return "Owner"
+        elif self.admin: return "Admin"
+        elif self.mod: return "Moderator"
+        else: return "Player"
+
+
---
+++ ServerBase.py
@@ -0,0 +1,103 @@
+__author__ = 'Scott Davey'
+
+"""
+Base for connecting and querying BFH/BFP4F/BF2 servers using RCON.
+Copyright (C) 2013  Scott Davey, www.sd149.co.uk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import random
+import socket
+import hashlib
+from Player import *
+
+class ServerBase:
+    """server object for BFH/BFP4F servers..."""
+    def __init__(self, ip, port, password):
+        self.socket = ""
+        self.ip = ip
+        self.port = int(port)
+        self.password = password
+        self.version = 0.0
+
+    def __sendFlag(self, c):
+        """
+        Sends to server including a flag so we can tell when the group of data has ended
+        """
+        c = "\2"+c+"\n"
+        self.socket.send(c.encode("utf-8"))
+
+    def __send(self, c):
+        """
+        Sends to server
+        """
+        c += "\n"
+        self.socket.send(c.encode("utf-8"))
+
+    def __recvFlag(self):
+        """
+        Collect data if in multiple packets
+        """
+        result = self.socket.recv(256).decode("utf-8")
+        while result[-3:] != "x04":
+            moreData = self.socket.recv(256).decode("utf-8")
+            result += moreData
+        return result[:-4]
+
+    def __recv(self):
+        """
+        Collect data from last request
+        """
+        r = self.socket.recv(256).decode("utf-8")
+        return r
+
+    def query(self, command, multi=False):
+        """
+        Query the server with an RCON command, set multi to True if there is a large amount of data
+        command: String, excluding linebreak
+        multi: Bool, used for receiving multiple packets
+        """
+        if not multi:
+            self.__send(command)
+            return self.__recv()
+        else:
+            self.__sendFlag(command)
+            return self.__recvFlag()
+
+
+    def connect(self): #TODO should use the new send and recv functions
+        msg = ""
+        mySocket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = mySocket
+        self.socket.connect ( (self.ip, self.port) )
+        msg = self.socket.recv(128).decode("utf-8") #welcome message, unusued
+        self.version = float(msg[-5:-2])
+        digest = self.socket.recv(128).decode("utf-8")
+        digest = digest[17:33]
+        key = digest + self.password
+        key = md5(key)
+        login = 'login {0}\n'.format(key)
+        login = login.encode("utf-8")
+        self.socket.send(login)
+        msg = mySocket.recv(100).decode("utf-8")
+        return msg
+
+def md5(dig):
+    md5 = hashlib.md5() #make a new md5 object
+    dig = dig.encode("utf-8") #coverting string to bytes
+    key = md5.update(dig) #md5'ing it all
+    key = md5.hexdigest()
+    key = str(key)
+    return key
